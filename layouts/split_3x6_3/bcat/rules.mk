698d002c453439712d7e2ddefd93a44205aafa01
Jonathan Rascher <jon@bcat.name>
2024-01-06 17:51:53 -0600

    Fix Crkbd build broken by spurious RGB underglow

--- layouts/split_3x6_3/bcat/rules.mk
+++ layouts/split_3x6_3/bcat/rules.mk
@@ -4,5 +4,8 @@ ifeq ($(strip $(KEYBOARD)), crkbd/rev1)
 	BOOTLOADER = atmel-dfu  # Elite-C
 
 	OLED_ENABLE = yes  # dual 128x32 OLED screens
-	RGB_MATRIX_ENABLE = yes  # per-key RGB and underglow
+
+	# Per-key RGB and underglow
+	RGBLIGHT_ENABLE = no
+	RGB_MATRIX_ENABLE = yes
 endif
