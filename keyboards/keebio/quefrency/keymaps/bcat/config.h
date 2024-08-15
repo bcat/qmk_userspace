55e377066814c56a86bca6dc7e85775f5e7dff2c
Jonathan Rascher <jon@bcat.name>
2024-08-15 00:32:18 -0500

    Fix Quefrency keymap build

--- keyboards/keebio/quefrency/keymaps/bcat/config.h
+++ keyboards/keebio/quefrency/keymaps/bcat/config.h
@@ -17,8 +17,8 @@
 #pragma once
 
 /* Use an extra LED on the right side since it's wider on the 65% PCB. */
-#undef RGBLED_NUM
-#define RGBLED_NUM 17
+#undef RGBLIGHT_LED_COUNT
+#define RGBLIGHT_LED_COUNT 17
 
 /* Set up RGB lighting so it works with either side as master. */
 #undef RGBLED_SPLIT
